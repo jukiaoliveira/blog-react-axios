489ec3d51512c65523ec07a5e773fc1a9d70c178
jukiaoliveira <jurafaoliveira@icloud.com>
2025-06-30 17:05:03 -0300

    Configuração do React Router

--- src/main.jsx
+++ src/main.jsx
@@ -3,8 +3,30 @@ import { createRoot } from 'react-dom/client'
 import './index.css'
 import App from './App.jsx'
 
+import { createBrowserRouter, RouterProvider, Route } from 'react-router-dom'
+
+// páginas
+import Home from './routes/Home.jsx'
+import NewPost from './routes/NewPost.jsx'
+
+const router = createBrowserRouter([
+  {
+    element: <App />,
+    children: [
+      {
+        path: '/',
+        element: <Home />
+      },
+      {
+        path: '/new',
+        element: <NewPost />
+      }
+    ]
+  }
+])
+
 createRoot(document.getElementById('root')).render(
   <StrictMode>
-    <App />
+    <RouterProvider router={router} />
   </StrictMode>,
-)
+);
