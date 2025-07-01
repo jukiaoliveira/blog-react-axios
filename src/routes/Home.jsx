153dd6fbb65723d1fd77e09ce16b7d7aa8e0f0a8
jukiaoliveira <jurafaoliveira@icloud.com>
2025-07-01 17:48:48 -0300

    CSS da Home

--- src/routes/Home.jsx
+++ src/routes/Home.jsx
@@ -10,15 +10,14 @@ const Home = () => {
   const [posts, setPosts] = useState([]);
 
   const getPosts = async () => {
-    
     try {
+      const response = await axios.get(
+        "https://jsonplaceholder.typicode.com/posts"
+      );
 
-      const response = await axios.get("https://jsonplaceholder.typicode.com/posts");
-
-      const data = response.data
+      const data = response.data;
 
       setPosts(data);
-
     } catch (error) {
       console.log(error);
     }
@@ -28,18 +27,24 @@ const Home = () => {
     getPosts();
   }, []);
 
-  return <div>
-    <h1>Últimos Posts</h1>
-    {posts.lenght === 0 ? <p>Carregando...</p> : (
-      posts.map((post) => (
-        <div className="post" key={post.id}>
-          <h2>{post.title}</h2>
-          <p>{post.body}</p>
-          <Link to={'/posts/${post.id}'} className="btn">Ler mais</Link>
-        </div>
-      ))
-    )}
-  </div>;
+  return (
+    <div className="home">
+      <h1>Últimos Posts</h1>
+      {posts.lenght === 0 ? (
+        <p>Carregando...</p>
+      ) : (
+        posts.map((post) => (
+          <div className="post" key={post.id}>
+            <h2>{post.title}</h2>
+            <p>{post.body}</p>
+            <Link to={"/posts/${post.id}"} className="btn">
+              Ler mais
+            </Link>
+          </div>
+        ))
+      )}
+    </div>
+  );
 };
 
 export default Home;
