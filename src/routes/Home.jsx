9368f226bd928a48ca247e70ac6d6873568d22ff
jukiaoliveira <jurafaoliveira@icloud.com>
2025-07-01 17:40:39 -0300

    Exibindo posts na Home

--- src/routes/Home.jsx
+++ src/routes/Home.jsx
@@ -17,7 +17,7 @@ const Home = () => {
 
       const data = response.data
 
-      console.log(data);
+      setPosts(data);
 
     } catch (error) {
       console.log(error);
@@ -28,7 +28,18 @@ const Home = () => {
     getPosts();
   }, []);
 
-  return <div>Home</div>;
+  return <div>
+    <h1>Últimos Posts</h1>
+    {posts.lenght === 0 ? <p>Carregando...</p> : (
+      posts.map((post) => (
+        <div className="post" key={post.id}>
+          <h2>{post.title}</h2>
+          <p>{post.body}</p>
+          <Link to={'/posts/${post.id}'} className="btn">Ler mais</Link>
+        </div>
+      ))
+    )}
+  </div>;
 };
 
 export default Home;
