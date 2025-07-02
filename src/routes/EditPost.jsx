223324ff33c31fb1911202b812888dbee7b57162
jukiaoliveira <jurafaoliveira@icloud.com>
2025-07-02 12:10:51 -0300

    Edição do Post

--- src/routes/EditPost.jsx
+++ src/routes/EditPost.jsx
@@ -1,16 +1,43 @@
-import blogFetch from '../axios/config';
+import blogFetch from "../axios/config";
 
-import { useState, useEffect } from 'react';
+import { useState, useEffect } from "react";
 
-import { useParams, useNavigate } from 'react-router-dom';
+import { useParams, useNavigate } from "react-router-dom";
 
 const EditPost = () => {
-    const navigate = useNavigate();
+  const navigate = useNavigate();
 
-    const [title, setTitle] = useState('');
-    const [body, setBody] = useState('');
+  const [title, setTitle] = useState("");
+  const [body, setBody] = useState("");
 
-    const { id } = useParams();
+  const { id } = useParams();
+
+  const getPost = async () => {
+    try {
+      const response = await blogFetch.get(`/posts/${id}`);
+
+      const data = response.data;
+
+      setTitle(data.title);
+      setBody(data.body);
+    } catch (error) {
+      console.log(error);
+    }
+  };
+
+  const editPost =async(e) => {
+    e.preventDefault()
+
+    const post = {title, body, userId: 1};
+
+    await blogFetch.put(`/posts/${id}`, {
+        body: post,
+    })
+  }
+
+  useEffect(() => {
+    getPost();
+  }, []);
 
   return (
     <div className="new-post">
@@ -24,6 +51,7 @@ const EditPost = () => {
             id="title"
             placeholder="Digite o título"
             onChange={(e) => setTitle(e.target.value)}
+            value={title || ""}
           />
         </div>
         <div className="form-control">
@@ -33,12 +61,13 @@ const EditPost = () => {
             id="body"
             placeholder="Digite o conteúdo"
             onChange={(e) => setBody(e.target.value)}
+            value={body || ""}
           ></textarea>
           <input type="submit" value="Editar Post" className="btn" />
         </div>
       </form>
     </div>
-  )
-}
+  );
+};
 
-export default EditPost
+export default EditPost;
