8d7cd0eef6921ad3e6c76672a49e619d11add0a2
jukiaoliveira <jurafaoliveira@icloud.com>
2025-07-02 11:39:48 -0300

    Criando a página de administração

--- src/main.jsx
+++ src/main.jsx
@@ -9,6 +9,7 @@ import { createBrowserRouter, RouterProvider, Route } from 'react-router-dom'
 import Home from './routes/Home.jsx'
 import NewPost from './routes/NewPost.jsx'
 import Post from './routes/Post.jsx'
+import Admin from './routes/Admin.jsx'
 
 const router = createBrowserRouter([
   {
@@ -25,6 +26,10 @@ const router = createBrowserRouter([
       {
         path: '/posts/:id',
         element: <Post />
+      },
+      {
+        path: '/admin',
+        element: <Admin />
       }
     ]
   }
