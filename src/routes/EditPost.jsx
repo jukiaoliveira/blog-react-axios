d34f66a21b56b416de9651891f31591b3bd73332
jukiaoliveira <jurafaoliveira@icloud.com>
2025-07-02 11:57:21 -0300

    Criando a página de edição do Post

---
+++ src/routes/EditPost.jsx
@@ -0,0 +1,44 @@
+import blogFetch from '../axios/config';
+
+import { useState, useEffect } from 'react';
+
+import { useParams, useNavigate } from 'react-router-dom';
+
+const EditPost = () => {
+    const navigate = useNavigate();
+
+    const [title, setTitle] = useState('');
+    const [body, setBody] = useState('');
+
+    const { id } = useParams();
+
+  return (
+    <div className="new-post">
+      <h2>Editando: {title}</h2>
+      <form onSubmit={(e) => editPost(e)}>
+        <div className="form-control">
+          <label htmlFor="title">Título:</label>
+          <input
+            type="text"
+            name="title"
+            id="title"
+            placeholder="Digite o título"
+            onChange={(e) => setTitle(e.target.value)}
+          />
+        </div>
+        <div className="form-control">
+          <label htmlFor="body">Conteúdo:</label>
+          <textarea
+            name="body"
+            id="body"
+            placeholder="Digite o conteúdo"
+            onChange={(e) => setBody(e.target.value)}
+          ></textarea>
+          <input type="submit" value="Editar Post" className="btn" />
+        </div>
+      </form>
+    </div>
+  )
+}
+
+export default EditPost
